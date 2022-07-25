24c286d1b420d7564678e104c614c0db11316db1
Darlan Araujo <darlanparaujo@outlook.com>
2022-07-25 17:53:02 -0300

    Atualização #3
    O JS está bem adiantando. Já foi criado as funções que criam a estrutura HTML, salva os dados no BD, pega as informações do BD, evita a duplicidade de dados na tela, mostra os dados na tela, e limpa a caixa de texto do input.

--- README.md
+++ README.md
@@ -9,3 +9,13 @@
  Apesar de usar o vídeo como base para desenvolver o projeto, todo o HTML e CSS foi feito apenas olhando o modelo pronto, sem o uso de qualquer código pronto que foi disponibilizado no GitHub.
 
  O JavaScript foi criado usando todos os recursos que ele ensina no vídeo. Porém, foi aplicado algumas mudanças, não com o intuito de melhorar, mas apenas no sentido de entender e tentar aplicar meu proprio entendimento.
+
+BRIEF:
+Sistema web para criação de Lista de Tarefas (Todo List).
+O usuário poderá criar diversas tarefas digitando o título.
+Não há limites para quantidade de tarefas, ou de caracteres de texto,
+o sistema se adapta para manter sempre o mesmo padrão de exibição.
+Ele se ajusta a qualquer tamanho de tela.
+O usuário também pode marcar as tarefas como concluidas ou desmarca-las.
+Também é possível excluí-las.
+Todas as tarefas criadar ficam armazenadas no LocalStorage do navegador.
--- js/index.js
+++ js/index.js
@@ -1,13 +1,101 @@
+'use strict';
 
-const criarTarefa = (tarefa) => {
+// Variáveis globais;
+const btnEnviar = document.querySelector('#btn-enviar');
+const textTarefa = document.querySelector('#text-tarefa');
+const btnLimpar = document.querySelector('#btn-limpar');
+
+/** Banco de Dados Local;
+ * modelo temporário que será substituido pelo BD LocalStorage do navegador.
+ */
+let banco = [
+    {tarefa: 'Estudar JavaScript', status: ''},
+    {tarefa: 'Estudar Inglês', status: ''},
+    {tarefa: 'Estudar Java', status: 'checked'}
+];
+
+/** Acesso ao BD
+ * Essa função recebe a tarefa vinda do usuário e adiciona ao BD o nome da tarefa e o seu status
+ * @param {*} tarefa Nome da tarefa inserida pelo usuário
+ * @param {*} status Status da tarefa. Por padrão sempre vazio
+ */
+function setBanco(tarefa, status='') {
+    banco.push({tarefa: tarefa, status: status});
+}
+
+/** Pegando os dados do BD
+ * Essa função acessa o BD e faz a leitura dos dados.
+ * A cada loop ele adiciona os dados dentro da função criarTarefa() passando os itens como parametros.
+ */
+function getBanco() {
+    banco.forEach((item) => {
+        criarEstruturaTarefa(item.tarefa, item.status);
+    });
+}
+
+// const getBanco = () => {
+//     banco.forEach((item) => criarTarefa(item.tarefa, item.status));
+// }
+
+/** Função limpar lista do BD
+ * Essa função evita duplicidade da lista do BD.
+ * Sem ela, toda vez que a função getBanco() é chamada ele duplica a lista do BD.
+ * Ela acessa o elemento pai aonde estão os elementos label com os itens da tarefa.
+ * Faz um loop por esse elemento pai buscando item por item dentro dele, e eliminando cada item encontrado.
+ */
+function limparBD() {
+    const wrapperTarefa = document.querySelector('#wrapper-tarefa');
+
+    while(wrapperTarefa.firstChild) {
+        wrapperTarefa.removeChild(wrapperTarefa.lastChild);
+    }
+}
+
+/** Função limpar texto
+ * Essa função limpa o texto que estiver na caixa do input aonde o usuário digita o nome da tarefa.
+ * Ela é utilizada pelo botão limpar texto e pela função enviarTarefa().
+ */
+function limpaTexto() {
+    textTarefa.value = '';
+}
+
+/** Função enviar tarefa
+ * Essa função chama o setBanco() passando o parametro vindo da caixa de input da tela, que por sua vez salva essa informação no BD.
+ * Depois limpa o BD para evitar duplicidade de dados.
+ * Depois pega as informação do BD, que ao mesmo tempo cria a estrutura HTML.
+ * Por fim, limpa o texto na caixa do input na tela.
+ */
+function enviarTarefa() {
+    setBanco(textTarefa.value, '');
+    limparBD();
+    getBanco();
+    limpaTexto();
+}
+
+/** Construtor do HTML
+ * A função cria em uma variável um elemento (label) que vai ser o elemento pai de outros elementos HTML.
+ * Depois de criado esse elemento recebe a classe que define o estilo CSS.
+ * Por fim, ele recebe o conteúdo HTML, com os elementos e classes já definidos.
+ * @param {*} tarefa recebe o texto inserido pelo usuário na interface.
+ * @param {*} status Se preenchido (checked) ele habilita a estilização CSS para mostrar a tarefa como marcada.
+ */
+const criarEstruturaTarefa = (tarefa, status) => {
     const itemHTML = document.createElement('label'); // Crio na memoria um item pai;
     itemHTML.classList.add('lista-tarefa'); // Adiciona a classe para os estilos;
 
     itemHTML.innerHTML = `
-        <input class="checkbox" type="checkbox">
+        <input class="checkbox" type="checkbox" ${status}>
         <p class="texto">${tarefa}</p>
         <button class="btn-fechar">X</button>
     ` // Adiciono o conteúdo HTML dentro do item criado;
 
     document.querySelector('#wrapper-tarefa').appendChild(itemHTML); // Adiciono o item na forma de um bloco dentro do DOM;
 }
+
+// Comandos dos botões
+btnEnviar.addEventListener('click', enviarTarefa);
+btnLimpar.addEventListener('click', limpaTexto);
+
+
+// Iniciador do código;
+getBanco();
